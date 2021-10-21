48cdd60d0f460e68fed8296a431cc2989c2d4d9c
Ozawa <46550891+Ozawa333@users.noreply.github.com>
2021-10-21 23:11:46 +0900

    main.py
    This is the main file of transformer_g

---
+++ main.py
@@ -0,0 +1,403 @@
+import os
+import json
+import math
+import torch
+import time 
+import argparse
+import numpy as np
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as Data
+from transformers import GPT2TokenizerFast
+
+# python -m torch.distributed.launch --nproc_per_node=3 main.py
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "1,2,3"
+parser = argparse.ArgumentParser()
+parser.add_argument("--local_rank", type=int)
+args = parser.parse_args()
+torch.cuda.set_device(args.local_rank)  # before your code runs
+torch.distributed.init_process_group(backend='gloo') #nccl for linux, 
+
+# Transformer Parameters
+d_model = 512  # Embedding Size
+d_ff = 2048 # FeedForward dimension
+d_k = d_v = 64  # dimension of K(=Q), V
+n_layers = 6  # number of Encoder of Decoder Layer
+n_heads = 8  # number of heads in Multi-Head Attention
+batch_size = 16
+
+src_len = 512 # enc_input max sequence length
+tgt_len = 512 # dec_input(=dec_output) max sequence length
+src_vocab_size = 50257+1
+tgt_vocab_size = 50257+1
+
+
+tokenizer = GPT2TokenizerFast.from_pretrained("EleutherAI/gpt-neo-1.3B")
+
+
+"""dataset"""
+def make_dataset(filedir):
+    
+    enc_inputs = []
+    dec_inputs = []
+    dec_outputs = []
+    for folder in os.listdir(filedir):
+        with open(filedir+"/"+folder+"/question.txt","r", encoding="utf8") as f_ques:
+            question = "".join(f_ques.readlines())
+        with open(filedir+"/"+folder+"/solutions.json","r", encoding="utf8") as f_solu:
+            solutions = json.load(f_solu)
+            for solution in solutions:
+                enc_inputs.append(question)
+                dec_inputs.append("Start " + solution)
+                dec_outputs.append(solution + " End")
+    return enc_inputs, dec_inputs, dec_outputs
+
+enc_inputs,dec_inputs,dec_outputs = make_dataset("./apps_dataset/train")
+
+#print(enc_input)
+
+
+#enc_inputs = enc_inputs[:128]
+#dec_inputs = dec_inputs[:128]
+#dec_outputs = dec_outputs[:128]
+
+#inputs = ["SOS hello world !","how are you ? ? EOS"]
+tokenizer.add_special_tokens({'pad_token': 'PAD'})
+enc_inputs = tokenizer(enc_inputs, padding='max_length', truncation=True, max_length = 512, return_tensors="pt")['input_ids']
+dec_inputs = tokenizer(dec_inputs, padding='max_length', truncation=True, max_length = 512, return_tensors="pt")['input_ids']
+dec_outputs = tokenizer(dec_outputs, padding='max_length', truncation=True, max_length = 512, return_tensors="pt")['input_ids']
+#input_ids = tokenizer(outputs, return_tensors="pt").input_ids
+
+class MyDataSet(Data.Dataset):
+  def __init__(self, enc_inputs, dec_inputs, dec_outputs):
+    super(MyDataSet, self).__init__()
+    self.enc_inputs = enc_inputs
+    self.dec_inputs = dec_inputs
+    self.dec_outputs = dec_outputs
+
+  def __len__(self):
+    return self.enc_inputs.shape[0]
+
+  def __getitem__(self, idx):
+    return self.enc_inputs[idx], self.dec_inputs[idx], self.dec_outputs[idx]
+
+#loader = Data.DataLoader(MyDataSet(enc_inputs, dec_inputs, dec_outputs), batch_size, True)
+
+
+
+"""transformer"""
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        '''
+        x: [seq_len, batch_size, d_model]
+        '''
+        x = x + self.pe[:x.size(0), :]
+        return self.dropout(x)
+
+
+def get_attn_pad_mask(seq_q, seq_k):
+    '''
+    seq_q: [batch_size, seq_len]
+    seq_k: [batch_size, seq_len]
+    seq_len could be src_len or it could be tgt_len
+    seq_len in seq_q and seq_len in seq_k maybe not equal
+    '''
+    batch_size, len_q = seq_q.size()
+    batch_size, len_k = seq_k.size()
+    # eq(zero) is PAD token
+    pad_attn_mask = seq_k.data.eq(50257).unsqueeze(1)  # [batch_size, 1, len_k], True is masked
+    return pad_attn_mask.expand(batch_size, len_q, len_k)  # [batch_size, len_q, len_k]
+
+
+def get_attn_subsequence_mask(seq):
+    '''
+    seq: [batch_size, tgt_len]
+    '''
+    attn_shape = [seq.size(0), seq.size(1), seq.size(1)]
+    subsequence_mask = np.triu(np.ones(attn_shape), k=1) # Upper triangular matrix
+    subsequence_mask = torch.from_numpy(subsequence_mask).byte()
+    return subsequence_mask # [batch_size, tgt_len, tgt_len]
+
+
+class ScaledDotProductAttention(nn.Module):
+    def __init__(self):
+        super(ScaledDotProductAttention, self).__init__()
+
+    def forward(self, Q, K, V, attn_mask):
+        '''
+        Q: [batch_size, n_heads, len_q, d_k]
+        K: [batch_size, n_heads, len_k, d_k]
+        V: [batch_size, n_heads, len_v(=len_k), d_v]
+        attn_mask: [batch_size, n_heads, seq_len, seq_len]
+        '''
+        scores = torch.matmul(Q, K.transpose(-1, -2)) / np.sqrt(d_k) # scores : [batch_size, n_heads, len_q, len_k]
+        scores.masked_fill_(attn_mask, -1e9) # Fills elements of self tensor with value where mask is True.
+
+        attn = nn.Softmax(dim=-1)(scores)
+        context = torch.matmul(attn, V) # [batch_size, n_heads, len_q, d_v]
+        return context, attn
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self):
+        super(MultiHeadAttention, self).__init__()
+        self.W_Q = nn.Linear(d_model, d_k * n_heads, bias=False)
+        self.W_K = nn.Linear(d_model, d_k * n_heads, bias=False)
+        self.W_V = nn.Linear(d_model, d_v * n_heads, bias=False)
+#        self.W_V2
+        self.fc = nn.Linear(n_heads * d_v, d_model, bias=False)
+    def forward(self, input_Q, input_K, input_V, attn_mask):
+        '''
+        input_Q: [batch_size, len_q, d_model]
+        input_K: [batch_size, len_k, d_model]
+        input_V: [batch_size, len_v(=len_k), d_model]
+        attn_mask: [batch_size, seq_len, seq_len]
+        '''
+        residual, batch_size = input_Q, input_Q.size(0)
+        # (B, S, D) -proj-> (B, S, D_new) -split-> (B, S, H, W) -trans-> (B, H, S, W)
+        Q = self.W_Q(input_Q).view(batch_size, -1, n_heads, d_k).transpose(1,2)  # Q: [batch_size, n_heads, len_q, d_k]
+        K = self.W_K(input_K).view(batch_size, -1, n_heads, d_k).transpose(1,2)  # K: [batch_size, n_heads, len_k, d_k]
+        V = self.W_V(input_V).view(batch_size, -1, n_heads, d_v).transpose(1,2)  # V: [batch_size, n_heads, len_v(=len_k), d_v]
+
+        attn_mask = attn_mask.unsqueeze(1).repeat(1, n_heads, 1, 1) # attn_mask : [batch_size, n_heads, seq_len, seq_len]
+
+        # context: [batch_size, n_heads, len_q, d_v], attn: [batch_size, n_heads, len_q, len_k]
+        context, attn = ScaledDotProductAttention()(Q, K, V, attn_mask)
+        context = context.transpose(1, 2).reshape(batch_size, -1, n_heads * d_v) # context: [batch_size, len_q, n_heads * d_v]
+        output = self.fc(context) # [batch_size, len_q, d_model]
+        return nn.LayerNorm(d_model).cuda()(output + residual), attn
+
+
+class PoswiseFeedForwardNet(nn.Module):
+    def __init__(self):
+        super(PoswiseFeedForwardNet, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(d_model, d_ff, bias=False),
+            nn.ReLU(),
+            nn.Linear(d_ff, d_model, bias=False)
+        )
+    def forward(self, inputs):
+        '''
+        inputs: [batch_size, seq_len, d_model]
+        '''
+        residual = inputs
+        output = self.fc(inputs)
+        return nn.LayerNorm(d_model).cuda()(output + residual) # [batch_size, seq_len, d_model]
+
+
+class EncoderLayer(nn.Module):
+    def __init__(self):
+        super(EncoderLayer, self).__init__()
+        self.enc_self_attn = MultiHeadAttention()
+        self.pos_ffn = PoswiseFeedForwardNet()
+
+    def forward(self, enc_inputs, enc_self_attn_mask):
+        '''
+        enc_inputs: [batch_size, src_len, d_model]
+        enc_self_attn_mask: [batch_size, src_len, src_len]
+        '''
+        # enc_outputs: [batch_size, src_len, d_model], attn: [batch_size, n_heads, src_len, src_len]
+        enc_outputs, attn = self.enc_self_attn(enc_inputs, enc_inputs, enc_inputs, enc_self_attn_mask) # enc_inputs to same Q,K,V
+        enc_outputs = self.pos_ffn(enc_outputs) # enc_outputs: [batch_size, src_len, d_model]
+        return enc_outputs, attn
+
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super(Encoder, self).__init__()
+        self.src_emb = nn.Embedding(src_vocab_size, d_model)
+        self.pos_emb = PositionalEncoding(d_model)
+        self.layers = nn.ModuleList([EncoderLayer() for _ in range(n_layers)])
+
+    def forward(self, enc_inputs):
+        '''
+        enc_inputs: [batch_size, src_len]
+        '''
+        enc_outputs = self.src_emb(enc_inputs) # [batch_size, src_len, d_model]
+        enc_outputs = self.pos_emb(enc_outputs.transpose(0, 1)).transpose(0, 1) # [batch_size, src_len, d_model]
+        enc_self_attn_mask = get_attn_pad_mask(enc_inputs, enc_inputs) # [batch_size, src_len, src_len]
+        enc_self_attns = []
+        for layer in self.layers:
+            # enc_outputs: [batch_size, src_len, d_model], enc_self_attn: [batch_size, n_heads, src_len, src_len]
+            enc_outputs, enc_self_attn = layer(enc_outputs, enc_self_attn_mask)
+            enc_self_attns.append(enc_self_attn)
+        return enc_outputs, enc_self_attns
+
+
+class DecoderLayer(nn.Module):
+    def __init__(self):
+        super(DecoderLayer, self).__init__()
+        self.dec_self_attn = MultiHeadAttention()
+        self.dec_enc_attn = MultiHeadAttention()
+        self.pos_ffn = PoswiseFeedForwardNet()
+
+    def forward(self, dec_inputs, enc_outputs, dec_self_attn_mask, dec_enc_attn_mask):
+        '''
+        dec_inputs: [batch_size, tgt_len, d_model]
+        enc_outputs: [batch_size, src_len, d_model]
+        dec_self_attn_mask: [batch_size, tgt_len, tgt_len]
+        dec_enc_attn_mask: [batch_size, tgt_len, src_len]
+        '''
+        # dec_outputs: [batch_size, tgt_len, d_model], dec_self_attn: [batch_size, n_heads, tgt_len, tgt_len]
+        dec_outputs, dec_self_attn = self.dec_self_attn(dec_inputs, dec_inputs, dec_inputs, dec_self_attn_mask)
+        # dec_outputs: [batch_size, tgt_len, d_model], dec_enc_attn: [batch_size, h_heads, tgt_len, src_len]
+        dec_outputs, dec_enc_attn = self.dec_enc_attn(dec_outputs, enc_outputs, enc_outputs, dec_enc_attn_mask)
+        dec_outputs = self.pos_ffn(dec_outputs) # [batch_size, tgt_len, d_model]
+        return dec_outputs, dec_self_attn, dec_enc_attn
+
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+        self.tgt_emb = nn.Embedding(tgt_vocab_size, d_model)
+        self.pos_emb = PositionalEncoding(d_model)
+        self.layers = nn.ModuleList([DecoderLayer() for _ in range(n_layers)])
+
+    def forward(self, dec_inputs, enc_inputs, enc_outputs):
+        '''
+        dec_inputs: [batch_size, tgt_len]
+        enc_intpus: [batch_size, src_len]
+        enc_outputs: [batch_size, src_len, d_model]
+        '''
+        dec_outputs = self.tgt_emb(dec_inputs) # [batch_size, tgt_len, d_model]
+        dec_outputs = self.pos_emb(dec_outputs.transpose(0, 1)).transpose(0, 1).cuda() # [batch_size, tgt_len, d_model]
+        dec_self_attn_pad_mask = get_attn_pad_mask(dec_inputs, dec_inputs).cuda() # [batch_size, tgt_len, tgt_len]
+        dec_self_attn_subsequence_mask = get_attn_subsequence_mask(dec_inputs).cuda() # [batch_size, tgt_len, tgt_len]
+        dec_self_attn_mask = torch.gt((dec_self_attn_pad_mask + dec_self_attn_subsequence_mask), 0).cuda() # [batch_size, tgt_len, tgt_len]
+
+        dec_enc_attn_mask = get_attn_pad_mask(dec_inputs, enc_inputs) # [batc_size, tgt_len, src_len]
+
+        dec_self_attns, dec_enc_attns = [], []
+        for layer in self.layers:
+            # dec_outputs: [batch_size, tgt_len, d_model], dec_self_attn: [batch_size, n_heads, tgt_len, tgt_len], dec_enc_attn: [batch_size, h_heads, tgt_len, src_len]
+            dec_outputs, dec_self_attn, dec_enc_attn = layer(dec_outputs, enc_outputs, dec_self_attn_mask, dec_enc_attn_mask)
+            dec_self_attns.append(dec_self_attn)
+            dec_enc_attns.append(dec_enc_attn)
+        return dec_outputs, dec_self_attns, dec_enc_attns
+
+
+class Transformer(nn.Module):
+    def __init__(self):
+        super(Transformer, self).__init__()
+        self.encoder = Encoder().cuda()
+        self.decoder = Decoder().cuda()
+        self.projection = nn.Linear(d_model, tgt_vocab_size, bias=False).cuda()
+    def forward(self, enc_inputs, dec_inputs):
+        '''
+        enc_inputs: [batch_size, src_len]
+        dec_inputs: [batch_size, tgt_len]
+        '''
+        # tensor to store decoder outputs
+        # outputs = torch.zeros(batch_size, tgt_len, tgt_vocab_size).to(self.device)
+
+        # enc_outputs: [batch_size, src_len, d_model], enc_self_attns: [n_layers, batch_size, n_heads, src_len, src_len]
+        enc_outputs, enc_self_attns = self.encoder(enc_inputs)
+        # dec_outpus: [batch_size, tgt_len, d_model], dec_self_attns: [n_layers, batch_size, n_heads, tgt_len, tgt_len], dec_enc_attn: [n_layers, batch_size, tgt_len, src_len]
+        dec_outputs, dec_self_attns, dec_enc_attns = self.decoder(dec_inputs, enc_inputs, enc_outputs)
+        dec_logits = self.projection(dec_outputs) # dec_logits: [batch_size, tgt_len, tgt_vocab_size]
+        return dec_logits.view(-1, dec_logits.size(-1)), enc_self_attns, dec_self_attns, dec_enc_attns
+
+
+
+"""epoch"""
+model = Transformer().cuda()
+# model = Transformer()
+model = torch.nn.parallel.DistributedDataParallel(model, find_unused_parameters=True)
+criterion = nn.CrossEntropyLoss(ignore_index=0)
+optimizer = optim.SGD(model.parameters(), lr=1e-3, momentum =0.99)
+loader = Data.DataLoader(MyDataSet(enc_inputs, dec_inputs, dec_outputs), batch_size, shuffle=True)
+
+for epoch in range(30):
+    start = time.time()
+    for enc_inputs, dec_inputs, dec_outputs in loader:
+        '''
+        enc_inputs: [batch_size, src_len]
+        dec_inputs: [batch_size, tgt_len]
+        dec_outputs: [batch_size, tgt_len]
+        '''
+        #print(tokenizer.decode(enc_inputs[0]))
+        #print(tokenizer.decode(dec_inputs[0]))
+        #print(tokenizer.decode(dec_outputs[0]))
+        #print("----------------------------------------------")
+
+        enc_inputs, dec_inputs, dec_outputs = enc_inputs.cuda(), dec_inputs.cuda(), dec_outputs.cuda()
+        # enc_inputs, dec_inputs, dec_outputs = enc_inputs, dec_inputs, dec_outputs
+        # outputs: [batch_size * tgt_len, tgt_vocab_size]
+        outputs, enc_self_attns, dec_self_attns, dec_enc_attns = model(enc_inputs, dec_inputs)
+        loss = criterion(outputs, dec_outputs.view(-1))
+        # print('Epoch:', '%04d' % (epoch + 1), 'loss =', '{:.6f}'.format(loss))
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    end = time.time()
+    print('time: ',end-start)
+    print('Epoch:', '%04d' % (epoch + 1), 'loss =', '{:.6f}'.format(loss))
+    
+    if(epoch%5 == 0):
+        torch.save({
+                'epoch': epoch,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'loss': loss,
+                }, "./checkpoints/checkpoint_%s.ckp" %(str(epoch)))
+
+
+def greedy_decoder(model, enc_input, start_symbol):
+    """
+    For simplicity, a Greedy Decoder is Beam search when K=1. This is necessary for inference as we don't know the
+    target sequence input. Therefore we try to generate the target input word by word, then feed it into the transformer.
+    Starting Reference: http://nlp.seas.harvard.edu/2018/04/03/attention.html#greedy-decoding
+    :param model: Transformer Model
+    :param enc_input: The encoder input
+    :param start_symbol: The start symbol. In this example it is 'S' which corresponds to index 4
+    :return: The target input
+    """
+    enc_outputs, enc_self_attns = model.encoder(enc_input)
+    dec_input = torch.zeros(1, 0).type_as(enc_input.data)
+    terminal = False
+    start_symbol = start_symbol
+    next_symbol = start_symbol
+    end_symbol = tokenizer("End", return_tensors="pt")['input_ids']
+    end_symbol =  end_symbol.cuda()
+    
+    
+    #while not terminal:
+    for i in range(512):
+        dec_input=torch.cat([dec_input.detach(),torch.tensor([[next_symbol]],dtype=enc_input.dtype).cuda()],-1)
+        dec_outputs, _, _ = model.decoder(dec_input, enc_input, enc_outputs)
+        projected = model.projection(dec_outputs)
+        prob = projected.squeeze(0).max(dim=-1, keepdim=False)[1]
+        next_word = prob.data[-1]
+        next_symbol = next_word
+        #if next_symbol == tgt_vocab["EOS"]:
+        if(next_symbol == end_symbol):
+            terminal = True
+            break
+        # print(next_word)
+    return dec_input
+
+# Test
+enc_inputs = tokenizer("print \"hello world\"", return_tensors="pt")['input_ids']
+start_symbol = tokenizer("Start")['input_ids']
+enc_inputs = enc_inputs.cuda()
+greedy_dec_input = greedy_decoder(model, enc_inputs.view(1, -1), start_symbol=start_symbol[0])
+
+predict, _, _, _ = model(enc_inputs.view(1, -1), greedy_dec_input)
+predict = predict.data.max(1, keepdim=True)[1]
+
+print(predict.squeeze())
+print(tokenizer.decode(predict.squeeze()))
