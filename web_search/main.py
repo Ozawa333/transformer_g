a4afcb41156881f9ffd57106ea22b3827fa6271d
Ozawa <46550891+Ozawa333@users.noreply.github.com>
2022-05-19 02:20:11 +0900

    Update main.py

--- web_search/main.py
+++ web_search/main.py
@@ -1,11 +1,13 @@
 import json
 import requests
 
-# Currently use ENCODER_SEARCH_ENGINE_ID only
+# https://console.cloud.google.com/home/dashboard
+# https://developers.google.com/custom-search/v1/overview
+API_KEY_S = [
+            ]
 
-API_KEY = "AIzaSyDpnhTpbzPE_kw4-34E_ZNBwrff-pJNgo4"
-ENCODER_SEARCH_ENGINE_ID = "bd40f442cc1318d85"
-DECODER_EARCH_ENGINE_ID = "8e1edf37eecc27858"
+SEARCH_ENGINE_ID = "bd40f442cc1318d85"
+# DECODER_EARCH_ENGINE_ID = "8e1edf37eecc27858"
 query = "1"
 page = 1
 start = (page - 1) * 10 + 1
@@ -22,6 +24,7 @@ def make_json(name):
 
 
 def search(API_KEY, SEARCH_ENGINE_ID, query, start):
+    #print(API_KEY,SEARCH_ENGINE_ID, query, start)
     url = f"https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={SEARCH_ENGINE_ID}&q={query}&start={start}"
 
     # make the API request
@@ -39,30 +42,50 @@ def search(API_KEY, SEARCH_ENGINE_ID, query, start):
         return None
 
 
-def make_dict(name):
+def make_dict(name, API_KEY, SEARCH_ENGINE_ID):
     with open("./"+name+"_description.json", "r") as load_f:
         tokens = json.load(load_f)
     search_token = ""
     # print(tokens)
+
+    search_count = 0
     for i,token in enumerate(tokens):
-        # print(token)
+        searched_singnal = False
+        print("----------"+str(i)+"----------")
+        print(token)
 
         if(token.isdigit()):
             tokens[token] = token
+            print("digit")
+            continue
 
         if(isinstance(tokens[token], int)):
+            for searched_token, _ in tokens.items():
+                if((searched_token==token[1:] or searched_token[1:]==token) and ("Ġ" == searched_token[0] or "Ġ" == token[0]) and len(token)>1):
+                    tokens[token] = tokens[searched_token]
+                    print("searched")
+                    searched_singnal = True
+                    break
+
+            if(searched_singnal == True):
+                break
+
             if("Ġ" == token[0]):
                 search_token = token[1:]
-            print("----------"+str(i)+"----------")
-            print(token)
-            tokens[token] = search(API_KEY, ENCODER_SEARCH_ENGINE_ID, search_token, start)
+            else:
+                search_token = token
+
+            tokens[token] = search(API_KEY, SEARCH_ENGINE_ID, search_token, start)
+            search_count += 1
             print(tokens[token])
 
-        if(i%10 == 0):
-            #print(type(tokens))
-            tokens_json = json.dumps(tokens)
-            with open("./"+name+"_description.json", "w") as save_f:
-                save_f.write(tokens_json)
+        if(search_count > 99):
+            break
+
+    #print(type(tokens))
+    tokens_json = json.dumps(tokens)
+    with open("./"+name+"_description.json", "w") as save_f:
+        save_f.write(tokens_json)
 
 
 '''make'''
@@ -70,7 +93,8 @@ def make_dict(name):
 # make_json("outputs")
 
 '''search'''
-make_dict("inputs")
-# make_dict("outputs")
+for API_KEY in API_KEY_S:
+    #make_dict("inputs", API_KEY, SEARCH_ENGINE_ID)
+    make_dict("outputs", API_KEY, SEARCH_ENGINE_ID)
 
 
